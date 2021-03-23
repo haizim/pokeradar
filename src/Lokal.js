2c27809980d96acad65acde8a9188301dd4ec383
haizim <wahihasyim@gmail.com>
2021-03-23 22:05:43 +0700

    revisi savePoke

--- src/Lokal.js
+++ src/Lokal.js
@@ -6,42 +6,41 @@ class Lokal extends Component{
         let callName = prompt("Give "+pokeName+" name",pokeName);
         console.log("callName : "+callName);
         
-
-            if (callName == null){
-                alert("Whew, you let "+pokeName+" free")
+        if (callName == null){
+            alert("Whew, you let "+pokeName+" free")
+        }else{
+            if(!localStorage.getItem("myPoke")){
+                const pokeNew = [{
+                        "poke":pokeName, 
+                        "call":callName,
+                        "img":img,
+                        "type":tipe,
+                    }];
+                const countNew = 1;
+                localStorage.setItem("myPoke",JSON.stringify(pokeNew))
+                localStorage.setItem("countPoke",countNew)
             }else{
-                if(!localStorage.getItem("myPoke")){
-                    const pokeNew = [{
-                            "poke":pokeName, 
-                            "call":callName,
-                            "img":img,
-                            "type":tipe,
-                        }];
-                    const countNew = 1;
+                const pokeOld = JSON.parse(localStorage.getItem("myPoke"));
+
+                if(pokeOld.filter(lama => lama.call === callName).length === 0 ){
+                    const pokeNew = [
+                            ...pokeOld,
+                            {
+                                "poke":pokeName, 
+                                "call":callName,
+                                "img":img,
+                                "type":tipe,
+                            }
+                        ];
+                    const countNew = parseInt(localStorage.getItem("countPoke"))+1;
                     localStorage.setItem("myPoke",JSON.stringify(pokeNew))
                     localStorage.setItem("countPoke",countNew)
                 }else{
-                    const pokeOld = JSON.parse(localStorage.getItem("myPoke"));
-                    
-                    if(pokeOld.filter(lama => lama.call === callName).length === 0 ){
-                        const pokeNew = [
-                                ...pokeOld,
-                                {
-                                    "poke":pokeName, 
-                                    "call":callName,
-                                    "img":img,
-                                    "type":tipe,
-                                }
-                            ];
-                        const countNew = parseInt(localStorage.getItem("countPoke"))+1;
-                        localStorage.setItem("myPoke",JSON.stringify(pokeNew))
-                        localStorage.setItem("countPoke",countNew)
-                    }else{
-                        alert("There's pokemon called "+callName+" in your bag!");
-                        this.pokeSave(pokeName, img, tipe);
-                    }
+                    alert("There's pokemon called "+callName+" in your bag!");
+                    this.pokeSave(pokeName, img, tipe);
                 }
             }
+        }
 
        
         
